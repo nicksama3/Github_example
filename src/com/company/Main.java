1174096c1d22baf8839f767fe1a70fc0f76fd550
nicksama3 <nicks0000622@gmail.com>
2020-11-18 19:48:37 -0500

    This is my first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello IS147");
+	System.out.println("Yeeeeeeeeeeeeet");
+	System.out.println("Wack");
     }
 }
